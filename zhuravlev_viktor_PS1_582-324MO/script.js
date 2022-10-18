990fee555ee93ec02e5d3039276dae86b79d9cde
V1ti0 <2140218@cmontmorency.qc.ca>
2022-10-18 19:38:23 -0400

    changes

--- zhuravlev_viktor_PS1_582-324MO/script.js
+++ zhuravlev_viktor_PS1_582-324MO/script.js
@@ -105,7 +105,10 @@ let chaptersObj = {
 function goToChapter(chapterName) {
     console.log(chaptersObj[chapterName]["subtitle"])
     console.log(chaptersObj[chapterName]["text"])
-    document.querySelector("subtitle").innerHTML=chaptersObj[chapterName]["subtitle"]
-    document.querySelector("text").innerHTML=chaptersObj[chapterName]["text"]
+  
+    document.querySelector(".subtitle").innerHTML=chaptersObj[chapterName]["subtitle"]
+    document.querySelector(".text").innerHTML=chaptersObj[chapterName]["text"]
+    document.querySelector(".main").innerHTML= '<img src="'+chaptersObj[chapterName]["img"]+'­>"';
+    
 }
 
